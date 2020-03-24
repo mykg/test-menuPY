572c3b66d18397734a123569c1afcb8d582ffaa9
ghost-kaze <07sameersaini@gmail.com>
2020-03-24 19:05:47 +0530

    reduced the LOC in menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 os.system("tput setaf 1")
 print("\t\tWelcome to TUI menu of Automation")
@@ -15,28 +16,27 @@ def menu():
         Press 6: to exit
         """)
     
+dic_local = {1:"date",2:"cal",3:"ifconfig",5:"yum install httpd"} # dictionary for local commands 
+dic_remote = {1:"ssh {} date",2:"ssh {} cal",3:"ssh {} ifconfig",5:"ssh {} yum install httpd"} # dictionary for remote commands
 
 location = input("Where to run script (local/remote): ")
 
 if location == "local":
 
     menu()
+    
     ch = int(input("Enter your choice : "))
 
-    if ch==1:
-        os.system("date")
-    elif ch==2:
-        os.system("cal")
-    elif ch==3:
-        os.system("ifconfig")
-    elif ch==4:
+    if ch==4:
         username = input("Input username: ")
-        os.system("useradd {}".format(username))
-    elif ch==5:
-        os.system("yum install httpd")
-    else:
-        exit()
+        os.system("useradd {}".format(username)) # didn't change this cuz it's different from the others
 
+    elif ch==6:
+        exit() # cuz it's not a string but it's a function
+
+    else:
+        os.system(dic_local[ch]) # uses dic_local for reducing the redundant if statements 
+        
 elif location == "remote":
     
     IP = input("Input remote IP: ")
@@ -44,19 +44,13 @@ elif location == "remote":
     menu()
     ch = int(input("Enter your choice : "))
 
-    if ch==1:
-        os.system("ssh {} date".format(IP))
-    elif ch==2:
-        os.system("ssh {} cal".format(IP))
-    elif ch==3:
-        os.system("ssh {} ifconfig".format(IP))
-    elif ch==4:
+    if ch==4:
         username = input("Input username: ")
-        os.system("ssh {} useradd {}".format(IP,username))
-    elif ch==5:
-        os.system("ssh {} yum install httpd".format(IP))
+        os.system("ssh {} useradd {}".format(IP,username)) # didn't change this cuz it's different from the others
+    elif ch==6:
+        exit()# cuz it's not a string but it's a function
     else:
-        exit()
+        os.system(dic_remote[ch].format(IP)) # same thing as local
 
 else:
     print("Invalid choice")
@@ -65,4 +59,5 @@ else:
         os.system("python3 /root/project/menu.py")
     else:
         print("Exitting.....")
+        time.sleep(0.5)
         exit()
