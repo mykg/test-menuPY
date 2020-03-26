92eabcbf7aa7b2f5d8355f1bc9044c0ae312bbf8
root <root@localhost.localdomain>
2020-03-26 06:00:49 +0530

    some small changes

--- menutest.py
+++ menutest.py
@@ -1,104 +1,97 @@
 import os
 
-def run():
-  os.system("tput setaf 1")
-  print("\t\tWelcome to TUI menu of Automation")
-  os.system("tput setaf 7")
-  print("\t\t---------------------------------")
 
-  def menu():
-    print("""
-        Press 1: to see date
-        Press 2: to check cal
-        Press 3: to check ip etc
-        Press 4: to create user
-        Press 5: conf web server
-        Press 6: to exit
-        """)
+while True:
+    os.system("tput setaf 1")
+    print("\t\tWelcome to TUI menu of Automation")
+    os.system("tput setaf 7")
+    print("\t\t---------------------------------")
+
+    def menu(): # main menu where tasks are written
+      print("""
+          Press 1: to see date
+          Press 2: to check cal
+          Press 3: to check ip etc
+          Press 4: to create user
+          Press 5: conf web server
+          Press 6: to exit
+          """)
     
 
-  location = input("Where to run script (local/remote/exit): ")
+    location = input("Where to run script (local/remote/exit): ")
 
-  if location == "local":
+    if location == "local":
 
-    menu()
-    ch = int(input("Enter your choice : "))
-
-    if ch==1:
-        os.system("date")
-        run()
-    elif ch==2:
-        os.system("cal")
-        run()
-    elif ch==3:
-        os.system("ifconfig")
-        run()
-    elif ch==4:
-        username = input("Input username: ")
-        os.system("useradd {}".format(username))
-        run()
-    elif ch==5:
-        os.system("yum install httpd")
-        run()
-    elif ch==6:
-        exit()
-    else: 
-        print("Invalid Choice")
         menu()
+        ch = int(input("Enter your choice : "))
 
-  elif location == "remote":
+        if (ch) == 1:
+            os.system("date")
+        elif (ch) == 2:
+            os.system("cal")
+        elif (ch) == 3:
+            os.system("ifconfig")
+        elif (ch) == 4:
+            username = input("Input username: ")
+            os.system("useradd {}".format(username))
+        elif (ch) == 5:
+            os.system("yum install httpd")
+        elif (ch) == 6:
+            exit()
+        else: 
+            print("Invalid Choice")
+            menu()
+  
+
+    elif location == "remote":
     
-    IP = input("Input remote IP: ")
-    checkip(IP)
+        IP = input("Input remote IP: ")
+        # checkip(IP)
 
-    menu()
-    ch = int(input("Enter your choice : "))
+        menu()
+        ch = int(input("Enter your choice : "))
 
-    if ch==1:
-        os.system("ssh {} date".format(IP))
-        run()
-    elif ch==2:
-        os.system("ssh {} cal".format(IP))
-        run()
-    elif ch==3:
-        os.system("ssh {} ifconfig".format(IP))
-        run()
-    elif ch==4:
-        username = input("Input username: ")
-        os.system("ssh {} useradd {}".format(IP,username))
-        run()
-    elif ch==5:
-        os.system("ssh {} yum install httpd".format(IP))
-        run()
-    elif ch==6:
+        if ch==1:
+            os.system("ssh {} date".format(IP))
+        elif ch==2:
+            os.system("ssh {} cal".format(IP))
+        elif ch==3:
+            os.system("ssh {} ifconfig".format(IP))
+        elif ch==4:
+            username = input("Input username: ")
+            os.system("ssh {} useradd {}".format(IP,username))
+        elif ch==5:
+            os.system("ssh {} yum install httpd".format(IP))
+        elif ch==6:
+            exit()
+        else:
+            print("Invalid choice")
+            menu()
+  
+    elif location == "exit":
         exit()
+
     else:
         print("Invalid choice")
-        menu()
 
-  
-  elif location == "exit":
-      exit()
 
-  else:
-    print("Invalid choice")
-    cont = input("Want to continue (yes/no): ")
-    if cont == "yes":
-        os.system("python3 /root/project/menu.py")
+    cont = input("Press Y/y to continue...")
+    if cont == "y" or cont == "Y":
+        os.system("clear")
+        continue
     else:
-        print("Exitting.....")
         exit()
 
 
-def checkip(ip):
-    print("checking ip/hostname: ")
-    #temp = ''
-    #os.system("ping {}".format(ip)).append(temp)
-    #print(temp)
-    #if temp == True:
-     #   print("IP/hostname is good to go")
-    #else:
-     #   print("wrong IP/hostname")
-      #  run()
 
-run()
+
+'''def checkip(ip):
+    print("checking ip/hostname: ")
+    temp = ''
+    os.system("ping {}".format(ip)).append(temp)
+    print(temp)
+    if temp == True:
+       print("IP/hostname is good to go")
+    else:
+        print("wrong IP/hostname")
+     '''
